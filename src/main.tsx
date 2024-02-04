d34e7d4883560e758c6bbb767d52a399b9bfab2a
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-03 19:20:14 -0600

    set up router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import './index.css'
+import { RouterProvider} from 'react-router-dom'
+import { router } from './routes/Routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 )
