f53a743ec36b53f6bf06e2ba0ee9f9be209932ca
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-08 01:43:11 -0600

    join all reducers

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,6 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from './slices/usersSlice'
 import modalReducer from './slices/modalSlice'
 import currentUserReducer from './slices/currentUserSlice'
+import currentWeatherReducer from './slices/currentWeatherSlice'
+import airPollutionReducer from './slices/airPollutionSlice'
+import cityInfoReducer from './slices/cityInfoSlice'
+import cityPhotoReducer from './slices/cityPhotoSlice'
+import forecastDaysReducer from './slices/forecastDaySlice'
 import { combineReducers } from '@reduxjs/toolkit'
 import { thunk } from 'redux-thunk'
 
@@ -17,7 +22,12 @@ const persistConfig = {
 const rootReducer = combineReducers({
     usersState: usersReducer,
     modalState: modalReducer,
-    currentUserState: currentUserReducer
+    currentUserState: currentUserReducer,
+    currentWeatherState: currentWeatherReducer,
+    airPollutionState: airPollutionReducer,
+    cityInfoState: cityInfoReducer,
+    cityPhotoState: cityPhotoReducer,
+    forecastDaysState: forecastDaysReducer,
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
@@ -28,5 +38,8 @@ export const store = configureStore({
         serializableCheck: false
     }).concat(thunk)
 })
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 
 export const persistor = persistStore(store)
